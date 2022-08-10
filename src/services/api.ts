16b426bda51191e8a43e0478ce7323b0d4b21954
Fernando <f.luhis@gmail.com>
2022-08-10 09:17:39 -0300

    Terceira alteracao na url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dt-money-eta-blush.vercel.app/api/',
+  baseURL: 'https://dt-money-fluhis.vercel.app/api/',
 });
 
 export { api };
